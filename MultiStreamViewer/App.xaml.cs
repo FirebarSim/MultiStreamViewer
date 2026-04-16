8c067119624f2292fdf9d90f9d00aeb5fdf3bcba
Matt Burleigh <32907623+FirebarSim@users.noreply.github.com>
2026-04-16 18:39:39 +0100

    Initial commit

---
+++ MultiStreamViewer/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MultiStreamViewer
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+
+}
